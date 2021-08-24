a2a46f88ad3efe075015c27795362822bdcd10a0
zhoupw@gmail.com <zhoupw@gmail.com>
2021-08-24 17:59:23 +0800

    fix the import path

--- did/resolver.go
+++ did/resolver.go
@@ -23,7 +23,7 @@ type DidResolver interface {
 	//GetPubkeyString(did string ,index int)(string,error)
 	VerifySig(did string, index int, msg []byte, sig []byte) error
 	Sign(did string, index int, msg []byte) ([]byte, error)
-	VerifyPresentation(did string, index int, presentation string, trustRoots []string, requiredTypes []*modules.VCFilter) error
+	VerifyPresentation(did string, index int, presentation string, requiredTypes []*modules.VCFilter) error
 	VerifyCredential(did string, index int, credential string, trustedDIDs []string) error
 	GetCredentialJsons(presentation string) ([]string, error)
 }
