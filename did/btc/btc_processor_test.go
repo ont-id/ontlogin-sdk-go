79075dc6d9a95beed20135858653dfafbe474d10
lucas7788 <sishsh@163.com>
2024-01-11 17:57:41 +0800

    add rpc in starknet

--- did/btc/btc_processor_test.go
+++ did/btc/btc_processor_test.go
@@ -18,9 +18,12 @@ func TestBTCVerifySig(t *testing.T) {
 	//assert.Nil(t, bo)
 
 	pubKeyBytes, _ := hex.DecodeString("026887958bcc4cb6f8c04ea49260f0d10e312c41baf485252953b14724db552aac")
+	pubKeyBytes, _ = hex.DecodeString("0273fa0df3ffceeda23b0074d9fe83d9ee3a209fad6e4546fdec5ede39abcbb70d")
 	sigBytes, err := base64.StdEncoding.DecodeString("G6nd7IqQaU8kxNbUDCnGLf+lA5ZxJ9TVlNOoNSuQ6j1yD1lG/Y25h01yT7SNxW56IuGNRX8Eu4baQYzhU78Wa0o=")
 	assert.Nil(t, err)
-	message := "hello world~"
+	sigBytes, _ = base64.StdEncoding.DecodeString("IDtG3XPLpiKOp4PjTzCo/ng8gm4MFTTyHeh/DaPC1XYsYaj5Jr4h8dnxmwuJtNkPkH40rEfnrrO8fgZKNOIF5iM=")
+
+	message := "hello world"
 	err = nbp.VerifySig("", 0, []byte(message), sigBytes, pubKeyBytes)
 	assert.Nil(t, err)
 }
